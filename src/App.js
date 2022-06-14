f816f6aafc89bd6a4afa3076bac614f13ff8f17e
Yaroslav Lebedenko <nailheart4@gmail.com>
2022-06-14 15:26:37 +0300

    implemented filters logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import SingIn from "./components/SignIn/SignIn";
 import SignUp from "./components/SignUp/SignUp";
-import Filter from "./components/Filter/Filter";
+// import Filter from "./components/Filter/Filter";
 import Trips from "./components/Trips/Trips";
 import Trip from "./components/Trips/Trip";
 import Bookings from "./components/Bookings/Bookings";
@@ -19,6 +20,38 @@ import "./assets/css/style.css";
     /bookings - список бронювань поточного користувача
 */
 function App() {
+  const [search, setSearch] = useState('');
+  const [duration, setDuration] = useState('');
+  const [level, setLevel] = useState('');
+
+  function filterDuration(search, duration, level) {
+    let data = dataTrips;
+
+    if (search) {
+      data = data.filter((item) => item.title.toLocaleLowerCase() === search.toLocaleLowerCase());
+    }
+
+    if (level) {
+      data = data.filter((item) => item.level === level);
+    }
+
+    switch(duration) {
+      case '0_x_5':
+        data = data.filter((item) => item.duration < 5);
+        break;
+      case '5_x_10':
+        data = data.filter((item) => item.duration >= 5 && item.duration < 10);
+        break;
+      case '10_x':
+        data = data.filter((item) => item.duration >= 10);
+        break;
+      default:
+        break;
+    }
+
+    return data;
+  }
+
   return (
     <>
       <Header />
@@ -32,8 +65,34 @@ function App() {
           element= {
             <main>
               <h1 className="visually-hidden">Travel app</h1>
-              <Filter />
-              {dataTrips.length ? <Trips trips={dataTrips} /> : null}
+              <section className="trips-filter">
+                <h2 className="visually-hidden">Trips filter</h2>
+                <form className="trips-filter__form" autoComplete="off">
+                  <label className="trips-filter__search input">
+                    <span className="visually-hidden">Search by name</span>
+                    <input name="search" onChange={(e) => setSearch(e.currentTarget.value)} type="search" placeholder="search by title" />
+                  </label>
+                  <label className="select">
+                    <span className="visually-hidden">Search by duration</span>
+                    <select name="duration" onChange={(e) => setDuration(e.currentTarget.value)}>
+                      <option value="">duration</option>
+                      <option value="0_x_5">&lt; 5 days</option>
+                      <option value="5_x_10">5 &gt; 10 days</option>
+                      <option value="10_x">&ge; 10 days</option>
+                    </select>
+                  </label>
+                  <label className="select">
+                    <span className="visually-hidden">Search by level</span>
+                    <select name="level" onChange={(e) => setLevel(e.currentTarget.value)}>
+                      <option value="">level</option>
+                      <option value="easy">easy</option>
+                      <option value="moderate">moderate</option>
+                      <option value="difficult">difficult</option>
+                    </select>
+                  </label>
+                </form>
+              </section>
+              {dataTrips.length ? <Trips trips={filterDuration(search, duration, level)} /> : null}
             </main>
           }
         />
